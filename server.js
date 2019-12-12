271d4783cc92db738eeb6a335d198f962be9b8e8
Glitch (fcc-back-2) <none>
2019-12-12 09:44:13 +0000

    👁🏦 Updated with Glitch

--- server.js
+++ server.js
@@ -1,100 +1,33 @@
+// server.js
+// where your node app starts
+
 // init project
 var express = require('express');
-var path = require('path');
 var app = express();
 
-//Returns true if the input is a number
-var isNumeric = function(input) {
-  return !isNaN(parseFloat(input)) && isFinite(input);
-}
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-//Returns true if the given month is a month
-var validMonth = function(month) {
-  switch(month.toLower())
-      {
-    case "january":
-      break;
-    case "february":
-      break;
-    case "march":
-      break;
-    case "april":
-      break;
-    case "may":
-      break;
-    case "june":
-      break;
-    case "july":
-      break;
-    case "august":
-      break;
-    case "september":
-      break;
-    case "october":
-      break;
-    case "november":
-      break;
-    case "december":
-      break;
-  }
-}
-
-//Returns true if the format is unix, false if natural, and undefined if neither
-var isUnixTimestamp = function(time) {
-  if (isNumeric(time)) {
-    return true;
-  }
-  //Check date?
-  return false;
-}
-
-//Given a date or unix time, returns an object with both
-var convertTime = function(time) {
-  var isUnix = isUnixTimestamp(time);
-  if (isUnix === undefined) {
-      return { unix: null, natural: null };
-  }
-  if (isUnix) {
-    var date = new Date(parseInt(time));
-    return { unix: time, utc: trimDate(date.toDateString()) };
-  }
-  return  { unix: Date.parse(time), utc: time };
-}
-
-//Trims the day off the date. ex: Tuesday January 5, 1999 --> January 5, 1999
-var trimDate = function(dateStr) {
-  var i = 0;
-  while(i < dateStr.length && dateStr[i] !== " ")
-    {
-      i++;
-    }
-  i++;
-  return dateStr.substring(i);
-}
-
-//Use our public files
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-//handle empty
-app.get("/api/timestamp/", (req, res) => {
-  res.json({ unix: Date.now(), utc: Date() });
+const requestIp = require('request-ip');
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  const clientIp = requestIp.getClientIp(req); 
+  res.json({ ipaddress: clientIp, "language":"en-US,en;q=0.5" });
 });
 
-//Handle a specific request
-app.get("/api/timestamp/:data", function (req, res) {
-  //valid url request?
-  if (req.params && Object.keys(req.params).length > 0) {
-    res.send(JSON.stringify(convertTime(req.params.data)));
-  } else {
-    //user gave incorrect data
-    res.sendStatus(400);
-  }
-});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
