c3f70c32985a5280925a1ec68b82433e8b7b0f9e
Glitch (fcc-back-3) <none>
2019-12-16 08:21:37 +0000

    👨‍❤️‍👨💂 Updated with Glitch

--- server.js
+++ server.js
@@ -1,46 +1,59 @@
-// server.js
-// where your node app starts
+'use strict';
 
-// init project
-var requestLanguage = require('express-request-language');
-var cookieParser = require('cookie-parser');
 var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var dns = require('dns');
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+var app = express();
+const mongo_key = process.env.MONGO_DB;
+var Schema = mongoose.Schema;
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+// Basic Configuration 
+var port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/
+mongoose.connect(process.env.MONGO_DB,);
+
+
+// mongoose.connect(process.env.MONGOLAB_URI);
+// url SCHEMA
+var urlSchema = new Schema({
+  id: Number,
+  url: String,
+});
+var urlModel = mongoose.model('Url', urlSchema);
+
+
+app.use(cors());
+
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(bodyParser());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const requestIp = require('request-ip');
-var parser = require('accept-language-parser');
-
-
-// your first API endpoint... 
-app.get("/api/whoami", function (req, res) {
-  const clientIp = requestIp.getClientIp(req); 
-  res.json({ ipaddress: clientIp, "language": req.headers['accept-language'], "software": req.headers['user-agent'] });
-});
-
-
-//testing route
-app.route('/dh').get((req,res) => { 
-  res.json({ ipaddress: req.headers['user-agent'] });
+  
+// test API
+app.get("/api/hello", function (req, res) {
+  let original = req.body;
+  res.json({greeting: 'hello API'});
+  console.log(original);
 });
 
 
 
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+
+
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
 });
