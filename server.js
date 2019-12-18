8252b6ddfcce06999c5d5de1e3796e1a1516d392
Glitch (woolen-syringa) <none>
2019-12-18 11:16:01 +0000

    🤘🔔 Updated with Glitch - FCC 4

--- server.js
+++ server.js
@@ -1,94 +1,100 @@
-'use strict';
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const shortid = require('shortid');
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
-var dns = require('dns');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var sleep = require('sleep');
+const cors = require('cors')
 
-var app = express();
-
-// Basic Configuration 
-var port = process.env.PORT || 3000;
-
+const mongoose = require('mongoose')
 const URI = process.env.URI;
 var Schema = mongoose.Schema;
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
-let connection = mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }); 
-let urlSchema = new Schema({
-    "originalUrl": String,
-    "shortUrl": String 
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+let userSchema = new Schema({
+    _id:{
+            type: String,
+            default: shortid.generate
+        },
+    name: String
 })
 // Create Model
-let UrlShortener = mongoose.model('UrlShortener', urlSchema);
+let user = mongoose.model('ExerciseUser', userSchema);
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 
-app.use(cors());
-
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
-  
-let regex = RegExp(/(^(http):\/\/|^(https:\/\/))/);
 
-// URL Shortener endpoint:
-app.post("/api/shorturl/new", function(req, res) {
-  let url = req.body.url;
-  let host = url.replace(/(^\w+:|^)\/\//, "");
-  let newRequest = { originalUrl: url };
-  //count number of objects
-  let cn;
+//post new user
+app.post("/api/exercise/new-user", (req, res) => {
+  let newRequest = { name: req.body.username };
   
-  //check if posted url is correct and/or have correct format
-  if(!regex.test(url)){
-    res.json({error: "invalid URL"});
-  }else{
-    dns.lookup(host, (err, address, family) => {
-      if (err) {res.json({ error: "invalid URL" })};
-    })    
-  } 
-
-  //try find posted url in mongo DB
-  UrlShortener.findOne(newRequest,(err, data) => {
-    //if posted url is not in DB, create new record
+  //try find posted name in mongo DB
+  user.findOne(newRequest,(err, data) => {
+    //if posted name is not in DB, create new record
     if (data == null){
-      UrlShortener.find().then((data1) => {
-          //find without arguments will fetch all records and count is stored in variable. Then with add 1 (this is instead of auto-increment plugin)
-          cn = data1.length;
-          var newURL = new UrlShortener({originalUrl: url, shortUrl: cn + 1 });
-          newURL.save(function(err, datas) {if (err) return console.error(err);});
-          res.json({original_url: url, short_url: cn + 1 });
-      });    
+      var newUser = new user(newRequest);
+      newUser.save(function(err, datas) {
+        if (err) return console.error(err);
+        res.json(datas);
+      });
     }else{
-      res.json({original_url: data.originalUrl, short_url: data.shortUrl});
+      res.json({name :data.name, id: data._id, note: "already in DB"});
     }
-  })   
-    
+  })
+  
 })
 
-//visit shortened link saved in mongo DB
-app.get("/api/shorturl/:shortUrl", (req, res)=> {   
-  UrlShortener.findOne(req.params,(err, doc)=> {
-  err 
-    ? console.log(err) 
-    : doc !== null
-    ? res.redirect(doc.originalUrl)
-    : res.json({error: "link not found"})
-  })
- })
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
-});
+
+
+
+
+//show all
+app.get("/all", (req, res)=> { 
+  user.find().then((data) =>{
+    let htm = "<h1 style='color: blue'>List of already saved Users in DB:</h1> <table><tr><th style='width: 100px'>User</th><th>ID</th><tr/>";
+    for (var i = 0; i<data.length;i++){htm = htm + "<tr><td>" + data[i].name + "</td><td>" + data[i]._id +"</td></tr>"}
+    htm = htm + "</table>"
+    
+    res.set("Content-Type", "text/plain"); 
+        res.format({
+        "text/html": () => res.send(htm)
+    });
+  })
+})
+
+
+// Not found middleware
+app.use((req, res, next) => {
+  return next({status: 404, message: 'not found'})
+})
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+  let errCode, errMessage
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400 // bad request
+    const keys = Object.keys(err.errors)
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message
+  } else {
+    // generic or custom error
+    errCode = err.status || 500
+    errMessage = err.message || 'Internal Server Error'
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage)
+})
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
